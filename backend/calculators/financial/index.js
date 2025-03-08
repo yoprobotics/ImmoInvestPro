7b643661ce882f273f8aefd25efc68649735aa56
yoprobotics <yoprobotics@gmail.com>
2025-03-08 15:21:08 -0500

    Mise à jour de l'index pour inclure le calculateur de taxes de mutation

--- backend/calculators/financial/index.js
+++ backend/calculators/financial/index.js
@@ -5,9 +5,11 @@
 const liquidityCalculator = require('./liquidity_calculator');
 const napkinFlipCalculator = require('./napkin_flip_calculator');
 const napkinMultiCalculator = require('./napkin_multi_calculator');
+const transferTaxCalculator = require('./transfer_tax_calculator');
 
 module.exports = {
   liquidityCalculator,
   napkinFlipCalculator,
-  napkinMultiCalculator
+  napkinMultiCalculator,
+  transferTaxCalculator
 };
